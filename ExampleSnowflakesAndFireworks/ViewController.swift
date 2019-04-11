0c7baf6bea00b824c6edb9cc0140352cc5709f23
Ivan Kramarchuk <night.silf@gmail.com>
2019-04-11 10:31:34 +0300

    Initial Commit

---
+++ ExampleSnowflakesAndFireworks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExampleSnowflakesAndFireworks
+//
+//  Created by Ivan Kramarchuk on 11/04/2019.
+//  Copyright © 2019 homework.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
